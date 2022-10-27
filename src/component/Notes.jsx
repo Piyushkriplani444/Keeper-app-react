1c0639dff82e1a172af7e326a2054bbea2d412e2
Piyush Kriplani <piyushkriplani444@gmail.com>
2022-10-27 18:24:33 +0530

    Initial commit added header , footer and note jsx

---
+++ src/component/Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes(){
+    return (
+        <div className="note">
+          <h1>For the title</h1>
+          <p>For the content</p>
+        </div>
+    );
+}
+export default Notes;
